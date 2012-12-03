8e37c21bae2bface2b1fa332e6506ac57f2e09f1
garima sharma <sharma_garima27@yahoo.com>
2012-12-03 11:04:27 +0530

    fonts changed

---
+++ src/com/thoughtworks/orteroid/activities/ViewSectionActivity.java
@@ -0,0 +1,4 @@
+package com.thoughtworks.orteroid.activities;
+
+public class ViewSectionActivity {
+}
--- src/com/thoughtworks/orteroid/utilities/SectionListAdapter.java
+++ src/com/thoughtworks/orteroid/utilities/SectionListAdapter.java
@@ -2,6 +2,7 @@ package com.thoughtworks.orteroid.utilities;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +17,7 @@ public class SectionListAdapter extends ArrayAdapter<Section> {
 
     private final Context context;
     private final List<Section> sections;
-
+    View rowView;
     public SectionListAdapter(Context context, List<Section> objects){
         super(context, R.layout.section_view_row_layout, R.id.row_text,objects);
         this.context = context;
@@ -25,11 +26,14 @@ public class SectionListAdapter extends ArrayAdapter<Section> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
-//        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         LayoutInflater layoutInflater = ((Activity)context).getLayoutInflater();
-        View rowView = layoutInflater.inflate(R.layout.section_view_row_layout, parent, false);
+
+        rowView = layoutInflater.inflate(R.layout.section_view_row_layout, parent, false);
         TextView textView = (TextView) rowView.findViewById(R.id.row_text);
+       Typeface tf = Typeface.createFromAsset(getContext().getAssets(),"fonts/Handwritten_Crystal_v2.ttf");
+        textView.setTypeface(tf);
         textView.setText(sections.get(position).name());
         return rowView;
     }
+
 }
