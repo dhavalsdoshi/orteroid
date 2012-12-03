2ce21ae2b8a5a20923cadf097c4a5d9c5650c02c
garima sharma <sharma_garima27@yahoo.com>
2012-12-03 15:59:29 +0530

    [Garima/Trishna] Drop down menu for SectionView

--- src/com/thoughtworks/orteroid/activities/ViewBoardActivity.java
+++ src/com/thoughtworks/orteroid/activities/ViewBoardActivity.java
@@ -1,19 +1,21 @@
 package com.thoughtworks.orteroid.activities;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Spinner;
 import com.thoughtworks.orteroid.Callback;
 import com.thoughtworks.orteroid.R;
 import com.thoughtworks.orteroid.constants.Constants;
 import com.thoughtworks.orteroid.models.Board;
 import com.thoughtworks.orteroid.models.Section;
 import com.thoughtworks.orteroid.repositories.BoardRepository;
-import com.thoughtworks.orteroid.utilities.SectionListAdapter;
 
-public class ViewBoardActivity extends ListActivity implements Callback<Board> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewBoardActivity extends Activity implements Callback<Board> {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -27,8 +29,13 @@ public class ViewBoardActivity extends ListActivity implements Callback<Board> {
 
     @Override
     public void execute(Board board) {
-        ArrayAdapter<Section> adapter = new SectionListAdapter(this, board.sections());
-        ListView listView = (ListView)findViewById(android.R.id.list);
-        listView.setAdapter(adapter);
+        List<Section> spinnerArray = board.sections();
+        Spinner spinner = (Spinner)findViewById(R.id.spinnerForSections);
+        List<String> sectionNames = new ArrayList<String>();
+        for (Section section : spinnerArray) {
+            sectionNames.add(section.name());
+        }
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sectionNames);
+        spinner.setAdapter(spinnerArrayAdapter);
     }
 }
--- src/com/thoughtworks/orteroid/utilities/SectionListAdapter.java
+++ src/com/thoughtworks/orteroid/utilities/SectionListAdapter.java
@@ -18,19 +18,19 @@ public class SectionListAdapter extends ArrayAdapter<Section> {
     private final Context context;
     private final List<Section> sections;
     View rowView;
-    public SectionListAdapter(Context context, List<Section> objects){
-        super(context, R.layout.section_view_row_layout, R.id.row_text,objects);
+
+    public SectionListAdapter(Context context, List<Section> objects) {
+        super(context, R.layout.section_view_row_layout, R.id.row_text, objects);
         this.context = context;
         this.sections = objects;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        LayoutInflater layoutInflater = ((Activity)context).getLayoutInflater();
-
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = ((Activity) context).getLayoutInflater();
         rowView = layoutInflater.inflate(R.layout.section_view_row_layout, parent, false);
         TextView textView = (TextView) rowView.findViewById(R.id.row_text);
-       Typeface tf = Typeface.createFromAsset(getContext().getAssets(),"fonts/Handwritten_Crystal_v2.ttf");
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Handwritten_Crystal_v2.ttf");
         textView.setTypeface(tf);
         textView.setText(sections.get(position).name());
         return rowView;
