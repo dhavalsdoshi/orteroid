9ba10ac6264b73d08375e1a8b4f22a1ac62074d2
Ashwin <ashwinve@thoughtworks.com>
2012-12-05 12:48:55 +0530

    [Ashwin]Modified board test to junit 3 standards

--- test/src/com/thoughtworks/orteroid/models/BoardTest.java
+++ test/src/com/thoughtworks/orteroid/models/BoardTest.java
@@ -1,18 +1,18 @@
 package com.thoughtworks.orteroid.models;
 
-import junit.framework.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import junit.framework.TestCase;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class BoardTest {
+
+public class BoardTest extends TestCase{
     List<Section> listOfSections;
     List<Point> listOfPoints;
     Point firstPoint, secondPoint;
+    Board board;
 
-    @Before
+    @Override
     public void setUp() {
         listOfSections = new ArrayList<Section>() {{
             add(new Section("What went well", 1));
@@ -21,27 +21,21 @@ public class BoardTest {
 
         firstPoint = new Point(1, 112, "Hey ! This is a test");
         secondPoint = new Point(2, 301, "This is a test too");
-
+        board = new Board("test", 2, "to test board", listOfSections);
         listOfPoints = new ArrayList<Point>();
         listOfPoints.add(firstPoint);
         listOfPoints.add(secondPoint);
+        board.update(listOfPoints);
     }
 
-    @Test
-    public void shouldUpdateBoardIfPointsWereNotAlreadyPresent() {
-        Board board = new Board("test", 2, "to test board", listOfSections);
-        board.update(listOfPoints);
-
-        Assert.assertTrue(board.sections().get(0).contains(firstPoint));
-        Assert.assertTrue(board.sections().get(1).contains(secondPoint));
+    public void testShouldUpdateBoardIfPointsWereNotAlreadyPresent() {
+        assertTrue(board.sections().get(0).contains(firstPoint));
+        assertTrue(board.sections().get(1).contains(secondPoint));
     }
-    @Test
-    public void shouldNotUpdateBoardIfPointsWereAlreadyPresent() {
-        Board board = new Board("test", 2, "to test board", listOfSections);
-        board.update(listOfPoints);
 
-        Assert.assertTrue(board.sections().get(0).contains(firstPoint));
-        Assert.assertTrue(board.sections().get(1).contains(secondPoint));
+    public void testShouldNotUpdateBoardIfPointsWereAlreadyPresent() {
+        assertTrue(board.sections().get(0).contains(firstPoint));
+        assertTrue(board.sections().get(1).contains(secondPoint));
     }
 
 }
